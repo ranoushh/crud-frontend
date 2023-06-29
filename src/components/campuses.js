c9741f1c55db366ce2a0a6a48c099b4c7a518b86
rana <abedorana7@gmail.com>
2023-06-29 14:39:02 -0400

    setup basic component pages

--- src/components/campuses.js
+++ src/components/campuses.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default function campuses() {
+export default function Campuses() {
+
+    //need our allCampuses array.
+    //array will have the name, image, address, description,
+    //name and address cannot be null
+    //uncomment once connected with backend.
+
   return (
-    <div>campuses</div>
+    <div>
+        <h1>All Campuses</h1>
+
+
+        {/* {allCampuses.length > 0 ? (
+        <ul>
+          {allCampuses.map((campus, index) => (
+            <li key={index}>
+              <p>Campus: {campus}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No campuses registered.</p>
+      )} */}
+
+    </div>
   )
 }
--- src/components/students.js
+++ src/components/students.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
- function students() {
+export default function Students() {
+
+    //need our allStudents array.
+    //array will have the name, image, address, description,
+    //name and address cannot be null
+    //uncomment once connected with backend.
+
   return (
-    <div>students</div>
+    <div>
+        <h1>All Students</h1>
+
+
+        {/* {allStudents.length > 0 ? (
+        <ul>
+          {allStudents.map((campus, index) => (
+            <li key={index}>
+              <p>Campus: {campus}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No students registered.</p>
+      )} */}
+
+    </div>
   )
 }
-
-
-export default Students;
