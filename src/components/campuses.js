7ac404bc4037ec0a98fd797aaf11b1864034c9ec
rana <abedorana7@gmail.com>
2023-06-28 21:54:51 -0400

    set up campus component

--- src/components/campuses.js
+++ src/components/campuses.js
@@ -1,3 +1,10 @@
+import React from 'react'
+
+export default function campuses() {
+  return (
+    <div>campuses</div>
+  )
+}
 
 //loaded? loadAllCampuses : noCampuses
 //need to include add campus button on both 
