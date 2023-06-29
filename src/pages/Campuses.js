6e2635dfd9302a599649c1e6168eb0e392d4f85c
rana <abedorana7@gmail.com>
2023-06-29 13:26:06 -0400

    set up combined reducers and store, some minor routes

---
+++ src/pages/Campuses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Campuses() {
+  return (
+    <div>
+        <h1>All Campuses</h1>
+
+        {/* <button onClick = {} id = "addCampus"> Add Campus </button> */}
+    </div>
+  )
+};
