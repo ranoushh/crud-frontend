854c922c116040a46b2d556e2cde44e2101a556b
rana <abedorana7@gmail.com>
2023-07-02 15:57:56 -0400

    addcampus fully functioning and working ajax post request, updated routes

---
+++ src/components/AddCampus.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+
+function AddCampus (){
+    const [newCampus, setNewCampus] = useState({
+        campusName: "",
+        imageUrl: "",
+        address: "",
+        description: ""
+    });
+
+    // //clear form - didnt work
+    // function clear(){
+    //     let element = document.getElementById("form");
+    //     element.reset()
+    // }
+
+    function handleChange(event){
+        setNewCampus({ ...newCampus, [event.target.name]: event.target.value });
+    };
+
+    function handleSubmit(event) {
+        console.log(newCampus);
+        
+        try {
+            axios.post('http://localhost:8080/api/campus/addCampus', {
+                name: newCampus.campusName,
+                imageurl: newCampus.imageUrl,
+                address: newCampus.address,
+                description: newCampus.description
+            })
+            .then(response => {
+                console.log(response.data);
+
+                //clear form here after data is used 
+                setNewCampus({
+                    campusName: "",
+                    imageUrl: "",
+                    address: "",
+                    description: ""
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    
+    return(
+        <div>
+            <h1>Add a Campus:</h1>
+            <form id="form" >
+            <p></p>
+                College Name: <input type = "text" 
+                                id = "nameCollege" 
+                                name = "campusName"
+                                value={newCampus.campusName}
+                                onChange={handleChange} 
+                                placeholder="Enter Campus Name" /> 
+                                <p></p>
+                Image Url:    <input type = "text" 
+                                id = "imageUrl" 
+                                name = "imageUrl"
+                                value={newCampus.imageUrl}
+                                onChange={handleChange} 
+                                placeholder="Enter URL" /> 
+                                <p></p>
+                Address:      <input type = "text" 
+                                id = "address" 
+                                name = "address"
+                                value={newCampus.address}
+                                onChange={handleChange} 
+                                placeholder="Enter Address" /> 
+                                <p></p>
+                Description:  <input type = "text" 
+                                id = "description" 
+                                name = "description"
+                                value={newCampus.description}
+                                onChange={handleChange} 
+                                placeholder="Enter description" /> 
+                                <p></p>
+            </form>
+      
+        <p></p>
+
+            <button onClick={handleSubmit} id="submit">
+                Submit
+            </button>
+        </div>
+    )
+};
+
+export default AddCampus;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Home() {
   console.log("helloo");
   return (
-    <div>Home Page!</div>
+    <div>
+      <h1>Home Page!</h1>
+      
+    </div>
   );
 };
